c716b88a31444d35889175f14b3ebbf9bc3523e5
oms064 <otokonoko064@gmail.com>
2016-03-19 11:24:31 -0600

    Rotación y cambio de color cada x  tiempo.

--- ClassDemo/Assets/RotateAndColor.cs
+++ ClassDemo/Assets/RotateAndColor.cs
@@ -3,17 +3,29 @@ using System.Collections;
 
 public class RotateAndColor : MonoBehaviour 
 {
+	[Range(0.0f, 100.0f)]public float rotacion = 45.0f;
+	[Range(1.0f, 10.0f)] public float tiempo = 1.0f;
+	public Color[] colores;
+	private MeshRenderer r;
 
 	// Use this for initialization
 	void Start () 
-    {
-        StartCoroutine("RotateColor");
+	{
+		StartCoroutine("RotateColor");
+		r = this.GetComponent<MeshRenderer> ();
+
+
 	}
 
-    IEnumerator RotateColor()
-    {
-        yield return new WaitForSeconds(3f);
-        transform.Rotate(0,10,0);
-        GetComponent<MeshRenderer>().material.color = new Color(0,1,0,1);
-    }
+	IEnumerator RotateColor()
+	{
+		while (true) {
+			foreach (var color in colores) {
+				yield return new WaitForSeconds (tiempo);
+				transform.Rotate (0, rotacion, 0);
+				r.material.color = color;
+			}
+
+		}
+	}
 }
