862a9e6e090961b701e5ebdc83794f908eb1a08f
leopineda <leopined@gmail.com>
2016-03-28 18:31:34 -0600

    Modificación al giro y color del cubo

--- ClassDemo/Assets/RotateAndColor.cs
+++ ClassDemo/Assets/RotateAndColor.cs
@@ -12,9 +12,28 @@ public class RotateAndColor : MonoBehaviour
 
     IEnumerator RotateColor()
     {     
-        yield return new WaitForSeconds(5f);
-        transform.Rotate(-5,0,-15);
-        GetComponent<MeshRenderer>().material.color = new Color(0,0,1,1);
+        int x = 0;
+	    int y = 0;
+	    int z = 0;
+	    int rojo = 0;
+	    int verde = 0;
+	    int azul = 0;	
+	    System.Random rdm = new System.Random();
+
+	    while(true)
+	    {
+	    	x = rdm.Next(0,30);
+	    	y = rdm.Next(0,30);
+	    	z = rdm.Next(0,30);
+
+	    	rojo = rdm.Next(0,99);
+	    	verde = rdm.Next(100,199);
+	    	azul = rdm.Next(200,255);
+
+	    	yield return new WaitForSeconds(0.5f);
+            transform.Rotate(x,y,z);
+            GetComponent<MeshRenderer>().material.color = new Color(rojo,verde,azul,1);
+	    }   
     }
 }
 
